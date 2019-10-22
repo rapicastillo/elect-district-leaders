ff2b2fbb203772d6adc30b1d3d46c364797cb268
Rapi Castillo <rapi.castillo@gmail.com>
2019-10-22 13:01:02 -0400

    Design

--- src/containers/NewReformers.js
+++ src/containers/NewReformers.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import DetailsContainer from './DetailsContainer';
 import MapContainer from './MapContainer';
+import newReformers from '../assets/newReformers.png';
 
 const NewReformers = (props) => (
   <div id='cnt-new-reformers'>
+    <header>
+      <img src={newReformers} />
+      <h3>Election District finder</h3>
+    </header>
     <div className='cnt-nf-map'>
       <DetailsContainer {...props} />
       <MapContainer {...props} />
